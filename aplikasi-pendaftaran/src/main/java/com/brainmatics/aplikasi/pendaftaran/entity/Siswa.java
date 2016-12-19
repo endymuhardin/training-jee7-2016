e3c743982246f51adba0ea868fc80981c4bf49a1
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-19 15:08:53 +0700

    relasi many to one

--- aplikasi-pendaftaran/src/main/java/com/brainmatics/aplikasi/pendaftaran/entity/Siswa.java
+++ aplikasi-pendaftaran/src/main/java/com/brainmatics/aplikasi/pendaftaran/entity/Siswa.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
@@ -23,16 +25,21 @@ public class Siswa implements Serializable {
     private Long id;
     
     @NotNull @NotEmpty
-    @Column(unique = true)
     private String nama;
     
     @NotNull @NotEmpty @Email
+    @Column(unique = true)
     private String email;
     
     @NotNull @Past
     @Column(name = "tanggal_lahir")
     @Temporal(TemporalType.DATE)
     private Date tanggalLahir;
+    
+    @NotNull
+    @ManyToOne
+    @JoinColumn(name = "id_sekolah_asal")
+    private Sekolah asalSekolah;
 
     public Long getId() {
         return id;
