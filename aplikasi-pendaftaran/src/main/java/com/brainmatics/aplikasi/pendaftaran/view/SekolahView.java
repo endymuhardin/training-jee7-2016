e56da02b578f6fa4b0c889634278320b0e4abcf4
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-20 14:16:19 +0700

    implement save sekolah

--- aplikasi-pendaftaran/src/main/java/com/brainmatics/aplikasi/pendaftaran/view/SekolahView.java
+++ aplikasi-pendaftaran/src/main/java/com/brainmatics/aplikasi/pendaftaran/view/SekolahView.java
@@ -1,5 +1,5 @@
 package com.brainmatics.aplikasi.pendaftaran.view;
-
+ 
 import com.brainmatics.aplikasi.pendaftaran.entity.Sekolah;
 import com.brainmatics.aplikasi.pendaftaran.sessionbeans.SekolahFacade;
 import java.util.List;
@@ -13,10 +13,20 @@ public class SekolahView {
 
     @EJB
     private SekolahFacade sekolahFacade;
-    
+    private Sekolah sekolah = new Sekolah();
     
     public SekolahView() {
     }
+
+    public Sekolah getSekolah() {
+        return sekolah;
+    }
+
+    public void setSekolah(Sekolah sekolah) {
+        this.sekolah = sekolah;
+    }
+    
+    
     
     public List<Sekolah> getDaftarSekolah(){
         return sekolahFacade.findAll();
@@ -25,5 +35,11 @@ public class SekolahView {
     public String editSekolah(Sekolah s){
         System.out.println("Mengedit sekolah "+s.getNama());
         return "form?faces-redirect=true&id="+s.getId();
+    }
+    
+    public String simpan(){
+        sekolahFacade.create(sekolah);
+        sekolah = new Sekolah();
+        return "list?faces-redirect=true";
     }
 }
