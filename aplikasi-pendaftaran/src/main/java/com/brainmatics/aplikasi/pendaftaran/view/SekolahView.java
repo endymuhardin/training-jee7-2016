6d81c5fc9e03204ed2ba4f696fcc31028a578647
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-20 16:23:33 +0700

    implement edit sekolah

--- aplikasi-pendaftaran/src/main/java/com/brainmatics/aplikasi/pendaftaran/view/SekolahView.java
+++ aplikasi-pendaftaran/src/main/java/com/brainmatics/aplikasi/pendaftaran/view/SekolahView.java
@@ -2,23 +2,27 @@ package com.brainmatics.aplikasi.pendaftaran.view;
  
 import com.brainmatics.aplikasi.pendaftaran.entity.Sekolah;
 import com.brainmatics.aplikasi.pendaftaran.sessionbeans.SekolahFacade;
+import java.io.Serializable;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.inject.Named;
-import javax.enterprise.context.RequestScoped;
+import javax.enterprise.context.SessionScoped;
 
 @Named(value = "sekolahView")
-@RequestScoped
-public class SekolahView {
+@SessionScoped
+public class SekolahView implements Serializable {
 
     @EJB
     private SekolahFacade sekolahFacade;
-    private Sekolah sekolah = new Sekolah();
+    private Sekolah sekolah;
     
     public SekolahView() {
     }
 
     public Sekolah getSekolah() {
+        if(sekolah == null){
+            sekolah = new Sekolah();
+        }
         return sekolah;
     }
 
@@ -26,19 +30,23 @@ public class SekolahView {
         this.sekolah = sekolah;
     }
     
-    
-    
     public List<Sekolah> getDaftarSekolah(){
         return sekolahFacade.findAll();
     } 
     
     public String editSekolah(Sekolah s){
+        sekolah = s;
+        System.out.println("ID Sekolah "+s.getId());
         System.out.println("Mengedit sekolah "+s.getNama());
-        return "form?faces-redirect=true&id="+s.getId();
+        return "form?faces-redirect=true";
     }
     
     public String simpan(){
-        sekolahFacade.create(sekolah);
+        if(sekolah.getId() == null){
+            sekolahFacade.create(sekolah);
+        } else {
+            sekolahFacade.edit(sekolah);
+        }
         sekolah = new Sekolah();
         return "list?faces-redirect=true";
     }
