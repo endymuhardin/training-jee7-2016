b717ad373d0e264a7092c57c473e9f3dabe64ca5
Endy Muhardin <endy.muhardin@gmail.com>
2016-12-21 16:45:41 +0700

    contoh login logout

---
+++ aplikasi-pendaftaran-spring/src/main/java/com/brainmatics/pendaftaran/KonfigurasiSecurity.java
@@ -0,0 +1,29 @@
+package com.brainmatics.pendaftaran;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class KonfigurasiSecurity extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .withUser("endy").password("123").roles("USER");
+    }
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+	http
+		.authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+		.formLogin()
+                .and().logout();
+}
+}
+
--- aplikasi-pendaftaran-spring/src/main/java/com/brainmatics/pendaftaran/controller/SekolahController.java
+++ aplikasi-pendaftaran-spring/src/main/java/com/brainmatics/pendaftaran/controller/SekolahController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
@@ -19,17 +20,20 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class SekolahController {
     @Autowired private SekolahDao sekolahDao;
     
+    @PreAuthorize("permitAll")
     @RequestMapping("/api/sekolah/")
     @ResponseBody
     public Page<Sekolah> dataSekolahJson(Pageable page){
         return sekolahDao.findAll(page);
     }
     
+    @PreAuthorize("isAuthenticated()")
     @RequestMapping("/sekolah/list")
     public ModelMap dataSekolahHtml(Pageable page){
         return new ModelMap("daftarSekolah", sekolahDao.findAll(page));
     }
     
+    @PreAuthorize("hasRole('USER')")
     @RequestMapping(value = "/sekolah/form", method = RequestMethod.GET)
     public ModelMap tampilkanForm(@RequestParam(required = false, name = "id") Sekolah sekolah ){
         if(sekolah == null){
@@ -38,6 +42,7 @@ public class SekolahController {
         return new ModelMap(sekolah);
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @RequestMapping(value = "/sekolah/form", method = RequestMethod.POST)
     public String prosesForm(@ModelAttribute @Valid Sekolah s, BindingResult errors){
         if(errors.hasErrors()){
